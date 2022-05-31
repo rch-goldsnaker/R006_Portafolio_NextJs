6e78e4a9bfa54a7c98ce717f58b1e4a756db18e9
rch-goldsnaker <rch.goldsnaker@gmail.com>
2022-05-31 03:03:05 -0300

    Adding a new IoT project in cards

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -18,12 +18,12 @@ export const projects = [
     id: 1,
   },
   {
-    title: 'IoT Monitoring',
-    description:"Using Html, Javascript and CSS - It is a web site where a dashboard of temperature motor sensors are shown. This Front-end template can be integrated with a IoT Project for the temperature motor control and monitoring.This could help with the maintenance of the equipment and prevent future failures.",
-    image: '/images/iotmotor.PNG',
-    tags: ['Html', 'JavaScript','CSS'],
-    source: 'https://rch-goldsnaker.github.io/rch.goldsnaker/',
-    visit: 'https://github.com/rch-goldsnaker/rch.goldsnaker',
+    title: 'IoT Dashboard EMQX',
+    description:"This is a very interesting project, where it starts from the programming of the Esp8266 and Esp32Cam devices together with the DHT11 temperature and humidity sensor, going through the frontend with Nuxt, then to the Backend with NodeJs, using a database with MongoDB, and finally an MQTT broker using EMQX. In the link you can see the static version on github pages.",
+    image: '/images/dashboard-demo-iot.PNG',
+    tags: ['Nuxt', 'Node','Mongo','Emqx','Esp8266','Esp32Cam'],
+    source: 'https://rch-goldsnaker.github.io/iot-demo.github.io/login',
+    visit: 'https://github.com/rch-goldsnaker/iot-demo.github.io',
     id: 2,
   }
 ];
