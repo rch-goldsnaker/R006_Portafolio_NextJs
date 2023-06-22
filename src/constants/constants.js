849071f53c7adf2740751e77705c314f19763763
Roger Chung <84298258+rch-goldsnaker@users.noreply.github.com>
2023-06-21 21:42:05 -0300

    Adding new proyects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -21,24 +21,15 @@ export const projects = [
       "https://github.com/rch-goldsnaker/R005_Blog_NextJs-GraphQL-Tailwind",
     id: 1,
   },
-  // {
-  //   title: 'IoT Monitoring',
-  //   description:"Using Html, Javascript and CSS - It is a web site where a dashboard of temperature motor sensors are shown. This Front-end template can be integrated with a IoT Project for the temperature motor control and monitoring.This could help with the maintenance of the equipment and prevent future failures.",
-  //   image: '/images/iotmotor.PNG',
-  //   tags: ['Html', 'JavaScript','CSS'],
-  //   source: 'https://rch-goldsnaker.github.io/rch.goldsnaker/',
-  //   visit: 'https://github.com/rch-goldsnaker/rch.goldsnaker',
-  //   id: 2,
-  // },
-  // {
-  //   title: 'Machine Learning',
-  //   description:"Using TensorFlow.js - On this web page you can see how a simple problem can be solved using machine learning. You can also see how a model is trained and tested. Being a machine learning application, I tried to use the simplest technologies in order to improve performance.",
-  //   image: '/images/mlXor.JPG',
-  //   tags: ['Html', 'JavaScript','CSS','TensorFlow.js'],
-  //   source: 'https://rch-goldsnaker.github.io/TensorFlowJS_XOR_001/',
-  //   visit: 'https://github.com/rch-goldsnaker/TensorFlowJS_XOR_001',
-  //   id: 3,
-  // },
+  {
+    title: "Air Quality Monitor Iot",
+    description: "This application is a dashboard that shows the values obtained from various air sensors.It also helps the user to quickly identify the status of air quality.Within the app, the two air quality scales, such as PM2.5 and AQI, are displayed in the main part.then the temperature and pressure values are displayed.It also shows a table with the values obtained from the sensor.",
+    image: "/images/airIot.JPG",
+    tags: ["Angular", "TypeScript", "Angular Material"],
+    source: "https://github.com/rch-goldsnaker/Air_Quality_Monitor_Iot_App",
+    visit: "https://rch-goldsnaker.github.io/Air_Quality_Monitor_Iot_App/",
+    id: 2,
+  },
   {
     title: "Machine Learning",
     description:
@@ -49,7 +40,7 @@ export const projects = [
       "https://rch-goldsnaker.github.io/TensorFlowJS_knowledgeTransfer_003/",
     visit:
       "https://github.com/rch-goldsnaker/TensorFlowJS_knowledgeTransfer_003",
-    id: 2,
+    id: 3,
   },
   {
     title: "Machine Learning",
@@ -59,7 +50,7 @@ export const projects = [
     tags: ["Html", "JavaScript", "CSS", "TensorFlow.js"],
     source: "https://rch-goldsnaker.github.io/TensorFlowJS_Mobilenet_002/",
     visit: "https://github.com/rch-goldsnaker/TensorFlowJS_Mobilenet_002",
-    id: 3,
+    id: 4,
   },
   {
     title: "Cards Links",
@@ -69,7 +60,7 @@ export const projects = [
     tags: ["Angular", "TypeScript", "Supabase"],
     source: "https://github.com/rch-goldsnaker/card-links-app",
     visit: "https://card-links-app.vercel.app/",
-    id: 4,
+    id: 5,
   },
   {
     title: "Music Player",
@@ -78,8 +69,26 @@ export const projects = [
     tags: ["Angular", "TypeScript", "SCSS"],
     source: "https://github.com/rch-goldsnaker/music-reproductor-angular",
     visit: "https://music-reproductor-angular.vercel.app/",
-    id: 5,
+    id: 6,
+  }, 
+  {
+    title: 'IoT Monitoring',
+    description: "Using Html, Javascript and CSS - It is a web site where a dashboard of temperature motor sensors are shown. This Front-end template can be integrated with a IoT Project for the temperature motor control and monitoring.This could help with the maintenance of the equipment and prevent future failures.",
+    image: '/images/iotmotor.PNG',
+    tags: ['Html', 'JavaScript', 'CSS'],
+    source: 'https://rch-goldsnaker.github.io/rch.goldsnaker/',
+    visit: 'https://github.com/rch-goldsnaker/rch.goldsnaker',
+    id: 7,
   },
+  {
+    title: 'Machine Learning',
+    description: "Using TensorFlow.js - On this web page you can see how a simple problem can be solved using machine learning. You can also see how a model is trained and tested. Being a machine learning application, I tried to use the simplest technologies in order to improve performance.",
+    image: '/images/mlXor.JPG',
+    tags: ['Html', 'JavaScript', 'CSS', 'TensorFlow.js'],
+    source: 'https://rch-goldsnaker.github.io/TensorFlowJS_XOR_001/',
+    visit: 'https://github.com/rch-goldsnaker/TensorFlowJS_XOR_001',
+    id: 8,
+  }
 ];
 
 export const TimeLineData = [
@@ -89,4 +98,5 @@ export const TimeLineData = [
   { year: 2020, text: 'Worked as a Project Engineer in a manufacturing company', },
   { year: 2021, text: 'Started as IoT developer in freelance', },
   { year: 2021, text: 'Started as Flow Specialist in a software company', },
+  { year: 2022, text: 'Started as FullStack Developer in a software company', }
 ];
